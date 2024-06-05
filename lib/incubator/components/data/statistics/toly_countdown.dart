93862c2031da771e3318901bf5de740a97a2d0c9
acsweet <1395723441@qq.com>
2024-06-05 14:20:52 +0800

    perfect countdown

--- lib/incubator/components/data/statistics/toly_countdown.dart
+++ lib/incubator/components/data/statistics/toly_countdown.dart
@@ -9,25 +9,42 @@ import 'package:intl/intl.dart';
 ///
 
 class TolyCountdown extends StatefulWidget {
+  ///Countdown Title
   final String title;
-  final Duration value;
+
+  ///Countdown value
+  final Duration? value;
+
+  ///Countdown front slot
   final Widget? prefix;
+
+  ///Countdown front slot
   final Widget? suffix;
   final TextStyle? valueStyle;
   final TextStyle? titleStyle;
+
+  /// [format] Date formatting
   final String format;
+
+  ///[ finish] Called when the countdown is complete
   final VoidCallback? finish;
+
+  ///[endTime]Incoming countdown end time
+  final DateTime? endTime;
 
   const TolyCountdown(
       {super.key,
-      this.value = const Duration(seconds: 0),
+      this.value,
       this.prefix,
       this.suffix,
       required this.title,
       this.valueStyle,
       this.titleStyle,
       this.format = 'HH:mm:ss',
-      this.finish});
+      this.finish,
+      this.endTime})
+      : assert(value == null || endTime == null, "The target value and defined value cannot coexist at the same time"),
+        assert(value != null || endTime != null, "Countdown requires passing in the initial value");
 
   @override
   State<TolyCountdown> createState() => _TolyCountdownState();
@@ -40,17 +57,21 @@ class _TolyCountdownState extends State<TolyCountdown> {
   @override
   void initState() {
     super.initState();
-    currentValue = widget.value.inMilliseconds;
+    if (widget.value != null) {
+      currentValue = widget.value!.inMilliseconds;
+    } else if (widget.endTime != null) {
+      assert(widget.endTime!.millisecondsSinceEpoch > DateTime.now().millisecondsSinceEpoch,
+          "The target time is greater than the current time");
+      currentValue = widget.endTime!.millisecondsSinceEpoch - DateTime.now().millisecondsSinceEpoch;
+    }
     timer = Timer.periodic(const Duration(seconds: 1), _doTask);
   }
 
   @override
   Widget build(BuildContext context) {
-    TextStyle textStyle =
-        widget.titleStyle ?? const TextStyle(color: Colors.grey, fontSize: 12);
+    TextStyle textStyle = widget.titleStyle ?? const TextStyle(color: Colors.grey, fontSize: 12);
     TextStyle valueStyle = widget.valueStyle ?? const TextStyle();
-    String valueText = DateFormat(widget.format)
-        .format(DateTime.fromMillisecondsSinceEpoch(currentValue, isUtc: true));
+    String valueText = DateFormat(widget.format).format(DateTime.fromMillisecondsSinceEpoch(currentValue, isUtc: true));
 
     Widget child = Column(
       children: [
