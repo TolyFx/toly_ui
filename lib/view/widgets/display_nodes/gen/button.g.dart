1eca9a512b81880b750dfcde18e6a482aff246b4
toly <1981462002@qq.com>
2024-06-10 23:06:18 +0800

    avatar

---
+++ lib/view/widgets/display_nodes/gen/button.g.dart
@@ -0,0 +1,8 @@
+/// ===================================================
+/// Power By 张风捷特烈 --- Generated file. Do not edit.
+/// github: https://github.com/toly1994328
+/// ===================================================
+
+part of 'node.g.dart';
+
+Map<String, dynamic> get _buttonData => {"ButtonDemo1":{"title":"填充样式","desc":"按钮的填充样式。通过 FillButtonPalette 调色形成样式, 提供 elevation 设置按钮的阴影深度，默认为 0。","code":"assets/code_res/button_demo1.txt"},"ButtonDemo2":{"title":"边线样式","desc":"按钮的边线样式。通过 OutlineButtonPalette 调色形成样式, dashGap 可以指定虚线的间隔。","code":"assets/code_res/button_demo2.txt"},"ButtonDemo3":{"title":"具有图标的按钮","desc":"按钮的 child 可以通过 Wrap 组件包裹若干个孩子。","code":"assets/code_res/button_demo3.txt"},"ButtonDemo4":{"title":"禁用按钮","desc":"按钮的 onPressed 回调为 null 时，表示禁用按钮。","code":"assets/code_res/button_demo4.txt"}};
---
+++ lib/view/widgets/display_nodes/gen/tabs.g.dart
@@ -0,0 +1,8 @@
+/// ===================================================
+/// Power By 张风捷特烈 --- Generated file. Do not edit.
+/// github: https://github.com/toly1994328
+/// ===================================================
+
+part of 'node.g.dart';
+
+Map<String, dynamic> get _tabsData => {"TabsDemo1":{"title":"基础用法","desc":"通过 TolyTabs 展示标签页，可容纳 MenuMeta 列表展示条目，onSelect 回调中处理点击条目事件，可用于路由跳转或者状态切换。","code":"assets/code_res/tabs_demo1.txt"},"TabsDemo2":{"title":"禁用项与分割线等配置项","desc":"","code":"assets/code_res/tabs_demo2.txt"},"TabsDemo3":{"title":"图标与居中","desc":"MenuMeta 设置 icon 属性时，展示对应图标。通过 alignment: TabAlignment.center 可以将页签居中","code":"assets/code_res/tabs_demo3.txt"},"TabsDemo4":{"title":"首尾组件","desc":"通过 leading和tail 属性，可以设置左右的首尾组件：","code":"assets/code_res/tabs_demo4.txt"},"TabsDemo5":{"title":"自定义页签组件","desc":"通过 cellBuilder 可以自定义构建页签样式：","code":"assets/code_res/tabs_demo5.txt"},"TabsDemo6":{"title":"页签的添加和移除","desc":"通过 leading和tail 属性，可以设置左右的首尾组件：","code":"assets/code_res/tabs_demo6.txt"}};
